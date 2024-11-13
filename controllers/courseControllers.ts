8d5c48f9c11173ae8a67ad8deae62607451684b6
Dan Kingo <danieldejen23@gmail.com>
2024-11-13 08:28:27 +0300

    simplify controllers logic

---
+++ controllers/courseControllers.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import Course, { courses } from "..";
+
+export const getAllCourse = (_req: Request, res: Response) => {
+  res.send(courses);
+};
+
+export const getCourse = (req: Request, res: Response) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+
+  if (!course)
+    res.status(404).send(`ther is no course with id ${req.params.id}`);
+  res.send(course);
+};
+
+export const addCourse = (req: Request, res: Response) => {
+  const course: Course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+
+  res.send(course);
+};
+
+export const deleteCourse = (req: Request, res: Response) => {
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send(`course with id ${req.params.id} is not found!`);
+
+  let index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+};
+
+export const updateCourse = (req: Request, res: Response) => {
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send(`course with id ${req.params.id} is not found!`);
+
+  // update the course and send it to the client
+  course.name = req.body.name;
+  res.send(course);
+};
--- routes/courseRouter.ts
+++ routes/courseRouter.ts
@@ -1,25 +1,18 @@
 import { Router } from "express";
-import { courses } from "..";
 import { validateData } from "../middlewares/validationMiddleware";
 import schema from "../schemas/courseSchemas";
-import addCourse from "../controllers/addCourseController";
-import updateCourse from "../controllers/updateCourseController";
-import deleteCourse from "../controllers/deleteCourseController";
+import {
+  addCourse,
+  deleteCourse,
+  getAllCourse,
+  getCourse,
+  updateCourse,
+} from "../controllers/courseControllers";
 
 const router = Router();
 
-router.get("/courses", (_req, res) => {
-  res.send(courses);
-});
-
-router.get("/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-
-  if (!course)
-    res.status(404).send(`ther is no course with id ${req.params.id}`);
-  res.send(course);
-});
-
+router.get("/courses", getAllCourse);
+router.get("/courses/:id", getCourse);
 router.post("/courses", validateData(schema), addCourse);
 router.put("/courses/:id", validateData(schema), updateCourse);
 router.delete("/courses/:id", validateData(schema), deleteCourse);
