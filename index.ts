33158343c937bde07ac05d6263f224be9b328f8c
Dan Kingo <danieldejen23@gmail.com>
2024-11-13 09:52:02 +0300

    implement morgan logger

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import router from "./routes/courseRouter";
+import cors from "cors";
+import morgan from "morgan";
+import path, { dirname } from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
 const app = express();
 const port = process.env.PORT || 3000;
-// const schema = z.object({
-//   name: z.string().min(3, "name must be at least 3 characters long!"),
-// });
-
 export default interface Course {
   id: number;
   name: string;
@@ -16,83 +17,21 @@ export const courses: Course[] = [
   { id: 3, name: "course 3" },
 ];
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs", "access.log"),
+  {
+    flags: "a",
+  }
+);
+
 app.use(express.json());
+app.use(cors());
+app.use(morgan("combined", { stream: accessLogStream }));
+
 app.use("/api", router);
-// app.get("/api/courses", (_req, res) => {
-//   res.send(courses);
-// });
-// app.get("/api/courses/:id", (req, res) => {
-//   const course = courses.find((c) => c.id === parseInt(req.params.id));
-
-//   if (!course)
-//     res.status(404).send(`ther is no course with id ${req.params.id}`);
-//   res.send(course);
-// });
-
-// app.post("/api/courses", (req: Request, res: Response, next: NextFunction) => {
-//   try {
-//     const result = schema.parse(req.body);
-//     const course: Course = {
-//       id: courses.length + 1,
-//       name: result.name,
-//     };
-//     courses.push(course);
-
-//     res.send(course);
-//     next();
-//   } catch (error) {
-//     if (error instanceof ZodError) {
-//       console.log(error.errors);
-//       const errorMessages = error.errors.map((issue: any) => ({
-//         message: `${issue.path.join(".")} is ${issue.message}`,
-//       }));
-//       res.status(400).json({ error: "Invalid data", details: errorMessages });
-//     } else {
-//       res.status(500).json({ error: "Internal Server Error" });
-//     }
-//   }
-// });
-
-// app.put(
-//   "/api/courses/:id",
-//   (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//       let course = courses.find((c) => c.id === parseInt(req.params.id));
-//       if (!course)
-//         res.status(404).send(`course with id ${req.params.id} is not found!`);
-
-//       // update the course and send it to the client
-//       const result = schema.parse(req.body);
-//       course.name = result.name;
-//       res.send(course);
-//       next();
-//     } catch (err) {
-//       if (err instanceof ZodError) {
-//         const errorMessages = err.errors.map((issue: any) => ({
-//           message: `${issue.path.join(".")} is ${issue.message}`,
-//         }));
-//         res.status(400).json({ error: "Invalid data", details: errorMessages });
-//       } else {
-//         res.status(500).json({ error: "Internal Server Error" });
-//       }
-//     }
-//   }
-// );
-
-// app.delete(
-//   "/api/courses/:id",
-//   (req: Request, res: Response, next: NextFunction) => {
-//     let course = courses.find((c) => c.id === parseInt(req.params.id));
-//     if (!course)
-//       res.status(404).send(`course with id ${req.params.id} is not found!`);
-
-//     let index = courses.indexOf(course);
-//     courses.splice(index, 1);
-
-//     res.send(course);
-//     next();
-//   }
-// );
 app.listen(port, () => {
   console.log(`server is started at port ${port}`);
 });
