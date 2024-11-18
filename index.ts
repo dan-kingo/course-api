587949a94081abf7506fd1b647917676417fb164
Dan Kingo <=danieldejen23@gmail.com>
2024-11-18 16:21:25 +0300

    clean up entry file

--- index.ts
+++ index.ts
@@ -7,7 +7,6 @@ import path, { dirname } from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import mongoose from 'mongoose'
-import Course from "./models/courses";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -26,52 +25,6 @@ const accessLogStream = fs.createWriteStream(
 
 // conecting to db
 mongoose.connect('mongodb://localhost/playground').then(() => {dbDebug('connected to db')}).catch((err) => {dbDebug(err)})
-
-
-const course = new Course({
-  name: 'ASP.Net Course',
-  author: 'jack sparrow',
-  price: 22,
-  isPublished: true,
-  tags: ['C#', 'backend'],
-})
-
-const createCourse = async () => {
-  const result = await course.save()
-  dbDebug(result)
-}
-
-// get courses 
-
-
-const getCourses = async () => {
-  const courses = await Course.find({name: /.*net.*/i})
-                              .select({name: 1, tags: 1, author: 1})
-                              .sort({name: 1})
-                              .limit(10)
-  dbDebug(courses)
-}
-
-//update directly
-const updateCourse = async (id:string) => {
-  const result = await Course.findByIdAndUpdate(id, {
-    $set: {
-      name: 'React.js Course',
-      price: 30,
-      author: 'Dagmaros'
-    }
-  }, {new: true})
-  dbDebug(result)
-}
-
-// removing a document 
-
-const removeCourse = async (id:string) => {
-  const result = await Course.findByIdAndDelete(id, {new: true})
-  dbDebug(result)
-}
-// createCourse()
-
 
 
 app.use(express.json());
