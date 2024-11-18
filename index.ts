da76af1180c578715314d94aae34be034d68186a
Dan Kingo <=danieldejen23@gmail.com>
2024-11-18 14:20:48 +0300

    adding validation rule

--- index.ts
+++ index.ts
@@ -28,11 +28,11 @@ mongoose.connect('mongodb://localhost/playground').then(() => {dbDebug('connecte
 
 // create a schema 
 const courseSchema = new mongoose.Schema({
-  name: String,
-  author: String,
-  price: Number,
-  isPublished: Boolean,
-  tags: [String],
+  name: {type: String, required: true, minlength: 5, maxlength: 255},
+  author: {type: String, required: true,minlength: 5},
+  price: {type: Number, required: function() {return this.isPublished}},
+  isPublished: {type: Boolean, required: true},
+  tags: {type: Array, validate: {validator: function(v: Array<string>) {return v && v.length > 0}, message: 'A course should have at least one tag'}},
   date: {type: Date, default: Date.now},
 })
 // creating a model 
@@ -40,7 +40,7 @@ const Course = mongoose.model('Course', courseSchema)
 
 const course = new Course({
   name: 'ASP.Net Course',
-  author: 'Daboss',
+  author: 'jack sparrow',
   price: 22,
   isPublished: true,
   tags: ['C#', 'backend'],
@@ -80,7 +80,7 @@ const removeCourse = async (id:string) => {
   const result = await Course.findByIdAndDelete(id, {new: true})
   dbDebug(result)
 }
-removeCourse('673b12fdfd4824eb13fc2ed6')
+createCourse()
 
 
 
