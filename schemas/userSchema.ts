8fd337c7519c75084f225111044dfac5df67dce1
Dan Kingo <=danieldejen23@gmail.com>
2024-11-20 16:01:24 +0300

    add regex validation for user password

--- schemas/userSchema.ts
+++ schemas/userSchema.ts
@@ -3,7 +3,10 @@ import {z} from 'zod';
 const userScehma =  z.object({
     name: z.string().min(3, {message: 'Name must be at least 3 characters long'}).max(50,{message: 'Name must be at most 50 characters long'}),
     email: z.string().email({message: 'Invalid email address'}),
-    password: z.string().min(6, {message: 'Password must be at least 8 characters long'}).max(1024),
+    password: z.string()
+    .min(6, {message: 'Password must be at least 8 characters long'})
+    .max(1024)
+    .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/, {message: 'Password must contain at least one uppercase letter, one lowercase letter, and one number'})
 })
 
 export default userScehma;
